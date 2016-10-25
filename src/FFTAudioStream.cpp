4eedc1241e5c55c317031386e18790629044df7f
ViRGiL7 <virgil7g@gmail.com>
2016-10-25 23:29:13 +0400

    License

--- src/FFTAudioStream.cpp
+++ src/FFTAudioStream.cpp
@@ -1,6 +1,15 @@
+//  Copyright © 2016 Fatih Gazimzyanov. Contacts: virgil7g@gmail.com
+
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
 //
-// Created by ViRGiL7 on 13.10.2016.
+//          http://www.apache.org/licenses/LICENSE-2.0
 //
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
 
 #include <SFML/Audio.hpp>
 #include "FFTAudioStream.h"
